a3a309e1c9d3df441cd202d417c6a0a010385c49
Irene <grim6ace@gmail.com>
2024-06-07 06:32:54 +0300

    Commit 2

--- pig-latin.js
+++ pig-latin.js
@@ -1,10 +1,10 @@
-
 const inputText = document.getElementById("input-string");
-const btn = document.getElementById("submit-btn");
+const translateBtn = document.getElementById("submit-btn");
+const resetBtn = document.getElementById("reset-btn");
 const outputText = document.getElementById("output");
 
 function checkWord() {
-  const regex = /[aeiouAEIOU]/;
+  const regex = /^[aeiouAEIOU]/;
   let wordArray = inputText.value.split("");
 
   if(!inputText.value || !isNaN(inputText.value)) {
@@ -17,20 +17,37 @@ function checkWord() {
       const endings = ['hay', 'yay', 'nay', 'way'];
       const randomEnding = endings[(Math.floor(Math.random() * endings.length))];
       outputText.textContent = wordArray.join("") + randomEnding;
+      return;
 
     } else if (regex.test(wordArray[1])) {
       const consonant = wordArray.shift();
       wordArray.push(consonant);
       outputText.textContent = wordArray.join("") + 'ay';
+      return;
 
     } else if (regex.test(wordArray[x])) {
       const consonants = wordArray.splice(0, x).join("");
       wordArray.push(consonants);
       outputText.textContent = wordArray.join("") + 'ay';
+      return;
       
         }   
   }
   
 };
 
-btn.addEventListener("click", checkWord);
+function reset() {
+  inputText.value = '';
+}
+
+translateBtn.addEventListener("click", checkWord);
+resetBtn.addEventListener("click", reset);
+
+inputText.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkWord();
+  } else if (e.key === "Shift") {
+    reset();
+  }
+})
+
