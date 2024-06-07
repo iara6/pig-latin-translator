4d971e68de52e827c32d1ad15c49407da457ffa3
Irene <grim6ace@gmail.com>
2024-06-07 07:51:01 +0300

    Commit 3

--- pig-latin.js
+++ pig-latin.js
@@ -3,50 +3,49 @@ const translateBtn = document.getElementById("submit-btn");
 const resetBtn = document.getElementById("reset-btn");
 const outputText = document.getElementById("output");
 
-function checkWord() {
+function translate() {
   const regex = /^[aeiouAEIOU]/;
-  let wordArray = inputText.value.split("");
+  const endings = ['hay', 'yay', 'nay', 'way']; 
+  let words = inputText.value.split(" ");
 
   if(!inputText.value || !isNaN(inputText.value)) {
-    outputText.textContent = 'Type in a word';
+    outputText.textContent = 'Type in a word or phrase';
     return;
   }
 
-  for (let x = 0; x < wordArray.length; x++) {
-    if (regex.test(wordArray[0])) {
-      const endings = ['hay', 'yay', 'nay', 'way'];
-      const randomEnding = endings[(Math.floor(Math.random() * endings.length))];
-      outputText.textContent = wordArray.join("") + randomEnding;
-      return;
-
-    } else if (regex.test(wordArray[1])) {
-      const consonant = wordArray.shift();
-      wordArray.push(consonant);
-      outputText.textContent = wordArray.join("") + 'ay';
-      return;
-
-    } else if (regex.test(wordArray[x])) {
-      const consonants = wordArray.splice(0, x).join("");
-      wordArray.push(consonants);
-      outputText.textContent = wordArray.join("") + 'ay';
-      return;
-      
-        }   
-  }
+  let translatedWords = words.map(word => {
+    let wordArray = word.split("");
   
-};
+    if (regex.test(wordArray[0])) {
+      const randomEnding = endings[(Math.floor(Math.random() * endings.length))];
+      return wordArray.join("") + randomEnding;
+
+    } else {
+      for (let x = 0; x < wordArray.length; x++) {
+        if (regex.test(wordArray[x])) {
+          const consonants = wordArray.splice(0, x).join("");
+          wordArray.push(consonants);
+          return wordArray.join("") + 'ay';
+      }
+    }
+     return word + 'ay';   
+   }   
+});
+  outputText.textContent = translatedWords.join(" ");
+}
 
 function reset() {
   inputText.value = '';
+  outputText.textContent = '';
 }
 
-translateBtn.addEventListener("click", checkWord);
+translateBtn.addEventListener("click", translate);
 resetBtn.addEventListener("click", reset);
 
 inputText.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    checkWord();
-  } else if (e.key === "Shift") {
+    translate();
+  } else if (e.key === "Backspace") {
     reset();
   }
 })
