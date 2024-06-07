80578763ad6c681877ab0623e0d64145f867b6db
Irene <grim6ace@gmail.com>
2024-06-08 02:30:37 +0300

    Commit 4

--- pig-latin.js
+++ pig-latin.js
@@ -5,6 +5,7 @@ const outputText = document.getElementById("output");
 
 function translate() {
   const regex = /^[aeiouAEIOU]/;
+  const regexPunctuation = /[.,:;?!]$/;
   const endings = ['hay', 'yay', 'nay', 'way']; 
   let words = inputText.value.split(" ");
 
@@ -14,21 +15,29 @@ function translate() {
   }
 
   let translatedWords = words.map(word => {
-    let wordArray = word.split("");
+    let punctuation = ''; 
+    let wordWithoutPunctuation = word;
+
+    if (regexPunctuation.test(word)) {
+      punctuation = word.slice(-1);
+      wordWithoutPunctuation = word.slice(0, -1);
+    }
+
+    let wordArray = wordWithoutPunctuation.split("");
   
     if (regex.test(wordArray[0])) {
       const randomEnding = endings[(Math.floor(Math.random() * endings.length))];
-      return wordArray.join("") + randomEnding;
+      return wordArray.join("") + randomEnding + punctuation;
 
     } else {
       for (let x = 0; x < wordArray.length; x++) {
         if (regex.test(wordArray[x])) {
           const consonants = wordArray.splice(0, x).join("");
           wordArray.push(consonants);
-          return wordArray.join("") + 'ay';
+          return wordArray.join("") + 'ay' + punctuation;
       }
     }
-     return word + 'ay';   
+     return wordWithoutPunctuation + 'ay' + punctuation;   
    }   
 });
   outputText.textContent = translatedWords.join(" ");
@@ -45,7 +54,7 @@ resetBtn.addEventListener("click", reset);
 inputText.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     translate();
-  } else if (e.key === "Backspace") {
+  } else if (e.key === "Control") {
     reset();
   }
 })
